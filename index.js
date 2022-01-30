fabd0332b65019685ae84e0704052b5e016fa93c
PRANAV KATARIA <pranavkataria978@gmail.com>
2022-01-30 18:14:06 +0530

    Setup Static file access for layouts and pages

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
 
+app.use(expressLayouts);
+// extract style and scripts from sub pages into layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+
+app.use(express.static('./assets'));
 
 app.use('/',require('./routes'));
 
-
+//view engine setup
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port,function(err){
 
@@ -16,3 +26,4 @@ app.listen(port,function(err){
 
     console.log(`Server is running on port: ${port}`);
 });
+
