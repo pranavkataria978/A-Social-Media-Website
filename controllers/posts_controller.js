1daac1a91b5361b53f39f0ad8a9d52a893eb440f
PRANAV KATARIA <pranavkataria978@gmail.com>
2022-02-17 01:17:40 +0530

    Ability to add a post without refreshing whole page added using AJAX

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -5,12 +5,25 @@ module.exports.create = async function(req,res){
 
     try{
 
-        await Post.create({
+       let post =  await Post.create({
 
             content : req.body.content,
             user: req.user._id
         });
 
+
+        if(req.xhr){
+
+            return res.status(200).json({
+
+                data: {
+
+                    post:post
+                },
+
+                message: "Post Created"
+            })
+        }
         req.flash('success',"Post Publised");
         return res.redirect('back');
 
